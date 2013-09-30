7233eedb951eb1d1af894180e5347045abf1b84d
Christopher Petersen <chris@petersen.io>
2013-09-29 18:56:15 -0700

    Swtich from pkgdmg to appdmg

--- spec/classes/licecap_spec.rb
+++ spec/classes/licecap_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'licecap' do
   it do
     should contain_package('LICEcap').with({
-      :provider => 'pkgdmg',
+      :provider => 'appdmg',
       :source   => 'http://www.cockos.com/licecap/licecap123.dmg'
     })
   end
